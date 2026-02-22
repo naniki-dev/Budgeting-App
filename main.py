fa9e12b19d7e003dd37ee8df7852db30d3dbfb17
LethaboKgasi <lethabomonyuku@gmail.com>
2026-02-22 17:03:40 +0200

    Added files and psuedocode

---
+++ main.py
@@ -0,0 +1,19 @@
+def main():
+    choice = input("1. Add income. 2. Add expense. 3. View total.")
+
+    # if choice == 
+
+'''
+Add the file
+'''
+
+
+def add_expense():
+    pass
+
+def add_income():
+    pass
+
+def view_total():
+    pass
+
